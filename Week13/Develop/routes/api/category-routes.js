7e6899ee77a7d3c4357df6a59ef29d64c7f0019a
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2023-01-19 13:25:18 -0500

    Added comments to css file

--- Week13/Develop/routes/api/category-routes.js
+++ Week13/Develop/routes/api/category-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+// http://localhost:3001/api/categories/
 router.get('/', async(req, res) => {
   // find all categories
   // be sure to include its associated Products
@@ -20,7 +20,7 @@ router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
     if (!categoryData) {
@@ -36,7 +36,8 @@ router.get('/:id', async(req, res) => {
 });
 
 router.post('/', async(req, res) => { try{
-  const categoryData = await Category.create({category_id: req.body.category_id,});
+  const categoryData = await Category.create(
+     req.body);
   res.status(200).json(categoryData);
 } catch (err) {
     res.status(400).json(err);
@@ -44,9 +45,23 @@ router.post('/', async(req, res) => { try{
 
   // create a new category
 });
-
-router.put('/:id', (req, res) => {
+// http://localhost:3001/api/categories/1
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try{
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async(req, res) => {
--- Week13/Develop/routes/api/product-routes.js
+++ Week13/Develop/routes/api/product-routes.js
@@ -4,19 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'ProductTag' }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'ProductTag' }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,7 +45,8 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+ try{ 
+  const productData = await Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -40,11 +61,11 @@ router.post('/', (req, res) => {
       // if no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
+    .then((productTagIds) => res.status(200).json(productTagIds))}
+    catch(err) {
       console.log(err);
       res.status(400).json(err);
-    });
+    }
 });
 
 // update product
